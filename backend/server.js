3e7185eac2478bce034695fb48fa51a8ef7f7a48
vitoriademourabauer <vitoriademourabauer@gmail.com>
2026-08-13 13:48:20 -0300

    Exercício de aula: adicionado consulta de CEP pela API ViaCEP.

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,24 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/mensagem', (req, res) => {
-    res.json({texto: "Olá do Servidor!'"});
+    res.json({texto: "Olá do Servidor!"});
 })
 
-app.listen(3001);
+app.get('/cep/:cep', async (req, res) => {
+    const { cep } = req.params;
+    try {
+        const respostas = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const dados = await respostas.json();
+
+
+        console.log(dados.erro); 
+
+        if(dados.erro) return res.status(404).json({erro: "CEP não encontrado"});
+
+        res.status(200).json(dados);
+    } catch (err) {
+        res.status(500).json({erro: "Erro de comunicação com VIACEP"})
+    }
+});
+
+app.listen(3001); 
